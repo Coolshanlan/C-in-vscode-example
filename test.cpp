cd7b42166c76e1ae81c2e380edaf8cc29fa25c6d
Coolshanlan <joey0201020123@gmail.com>
2020-03-31 20:27:05 +0800

    .vscode config update

--- test.cpp
+++ test.cpp
@@ -4,8 +4,8 @@
 using namespace std;
 int main()
 {
-    cout<<"hello world1"<<endl;
-    cout<<"hello world2"<<endl;
-    cout<<"hello world3"<<endl;
+    cout << "hello world1" << endl;
+    cout << "hello world5" << endl;
+    cout << "hello world3" << endl;
     system("pause");
 }
